42b3824380528e3ef933d662674f60378371a435
songtao <songtao8132@126.com>
2017-06-27 23:07:19 +0800

    条件渲染

--- js/test2.js
+++ js/test2.js
@@ -1,6 +1,20 @@
 var app5 = new Vue({
 	el:'#app5',
 	data:{
-		ok:'a',
+		a:'asdfasdf',
+		ok:true,
+		type:'',
+		loginType:'username',
+		list:[{id:'aa',name:'aaN'},{id:'bb',name:'bbN'}],
+		obj:{id:'111',name:'Lilei',age:20}
+	},
+	methods:{
+		tran:function(){
+			if(this.loginType=='username'){
+				this.loginType='email'
+			}else if(this.loginType=='email'){
+				this.loginType='username';
+			}
+		}
 	}
 });
